7655fcc7fe4f8becf2e57b28da92fcfd28d4b83b
Merlin S <128160860+etu46508@users.noreply.github.com>
2023-04-27 15:35:28 +0200

    Add files via upload

---
+++ projetV2/projetV0/src/Controller/Controller.java
@@ -0,0 +1,4 @@
+package Controller;
+import Model.*;
+public class Controller {
+}
---
+++ projetV2/projetV0/src/View/Main.java
@@ -0,0 +1,10 @@
+package View;
+
+import View.Utility.MainWindow;
+
+public class Main {
+    public static void main(String[] args){
+        MainWindow mainWindow = new MainWindow();
+
+    }
+}
